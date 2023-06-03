30d479a49b8f0439d728c6de920768c7f5403864
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2023-06-04 00:16:28 +0900

    [Update] mod

--- p2_ct_project/p2ch12/prepcache_luna.py
+++ p2_ct_project/p2ch12/prepcache_luna.py
@@ -1,12 +1,14 @@
-import argparse
-import sys
+
 
 import os
+import sys
 
 # os.sepはプラットフォーム固有の区切り文字(Windows: `\`, Unix: `/`)
 module_parent_dir = os.sep.join([os.path.dirname(__file__), '..']) # p2_ct_project
 print("module_parent_dir", module_parent_dir)
 sys.path.append(module_parent_dir)
+
+import argparse
 
 import numpy as np
 
